9e1c9965365ab7b3aa32407435b93ae6ed209b16
shivanandpg <shivanandshivanad2095@gmail.com>
2025-04-20 16:54:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,47 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, render_template, redirect, url_for, request,jsonify;
+
 
 app = Flask(__name__)
+signal_status = {'state': 'stopped'}  # Possible: 'stopped', 'starting', 'started'
+class ComndServer:
+    commnd = "stop"
 
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return render_template('index.html', state=signal_status['state'])
+
+@app.route('/app/start')
+def start():
+    ComndServer.commnd = "start"
+    signal_status['state'] = 'starting'
+    # return redirect(url_for('index'))
+    return jsonify({'status': signal_status['state']})
+
+@app.route('/app/refresh')
+def refresh():
+    return jsonify({'Content-Type': 'application/json', 'status': signal_status['state']})
+
+@app.route('/board/getCom')
+def getCom():
+    return ComndServer.commnd
+
+
+@app.route('/board/updateState')
+def start_done():
+    status = request.args.get('state', 'unknown')
+    print(status)
+    signal_status['state'] = status
+    return "OK"
+
+
+
+@app.route('/app/stop')
+def stop():
+    ComndServer.commnd = "stop"
+    signal_status['state'] = 'stopping'
+    # return redirect(url_for('index'))
+    return jsonify({'status': signal_status['state']})
 
 
 if __name__ == '__main__':
